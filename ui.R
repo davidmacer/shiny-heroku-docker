67496d32982eb4d598e007bc13e55d325bdfa6ac
davidmacer <jdavid.macer@gmail.com>
2022-08-24 16:02:55 -0500

    Cambia el título solo como cotorreo

--- ui.R
+++ ui.R
@@ -13,7 +13,7 @@ library(shiny)
 shinyUI(fluidPage(
 
     # Application title
-    titlePanel("Old Faithful Geyser Data"),
+    titlePanel("Hola, Donají!! Hay que subir las apps a Heroku!"),
 
     # Sidebar with a slider input for number of bins
     sidebarLayout(
